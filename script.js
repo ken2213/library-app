961ea8ff453eef681dcf4acf6b96ec9f3418766a
Kenneth Ornieta <me@kennethornieta.com>
2022-09-28 20:46:37 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let myLibrary = [];
+
+
+
+
+
+
+function Book() {
+  // the constructor...
+}
+
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+var modalContainerHide = document.getElementById('modal-container-hide');
+
+window.onclick = function(event) {
+  if (event.target == modalContainer) {
+    modalContainerHide.style.display = "none";
+  }
+}
+
+
