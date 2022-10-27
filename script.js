a89fa9df08084ffb7113a7952cb906d650b4875c
Kenneth Ornieta <me@kennethornieta.com>
2022-10-27 12:17:58 +0800

    book items for library

--- script.js
+++ script.js
@@ -1,25 +1,71 @@
-let myLibrary = [];
+
+const books = document.querySelector(".books");
+
+const myLibrary = [{
+  title: 'Book1',
+  author: 'me',
+  pages: 500,
+  read: true
+},{
+  title: 'Book2',
+  author: 'you',
+  pages: 500,
+  read: true
+}];
 
 
-
-
-
-
-function Book() {
-  // the constructor...
+// todo: Creates an HTML Element inside the Book
+function createBookElement(el, content, className) {
+  const element = document.createElement(el);
+  element.textContent = content;
+  element.setAttribute("class", className);
+  return element;
 }
 
 
-function addBookToLibrary() {
-  // do stuff here
+// todo: Creates a Book Item for the library
+function createBookItem (book, index) {
+  const bookItem = document.createElement('div');
+  bookItem.setAttribute('id', index);
+  bookItem.setAttribute('key', index);
+  bookItem.setAttribute('class', 'card book');
+  bookItem.appendChild(
+    createBookElement("h1", `Title: ${book.title}`, "book-title")
+  );
+  bookItem.appendChild(
+    createBookElement("h1", `Author: ${book.author}`, "book-author")
+  );
+  bookItem.appendChild(
+    createBookElement("h1", `Pages: ${book.pages}`, "book-pages")
+  );
+  books.insertAdjacentElement('afterbegin', bookItem) //todo: inserts the "bookItem" inside the "books" class
 }
 
-var modalContainerHide = document.getElementById('modal-container-hide');
 
-window.onclick = function(event) {
-  if (event.target == modalContainer) {
-    modalContainerHide.style.display = "none";
-  }
+// todo: Renders the books of the library to the webpage
+function renderBooks () {
+  myLibrary.map((book, index) => {
+    createBookItem(book, index)
+  })
 }
+
+
+renderBooks();
+
+
+
+// function Book(title, author, pages, read) {
+//   // the constructor...
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+// }
+
+
+// function addBookToLibrary() {
+//   // do stuff here
+// }
+
 
 
